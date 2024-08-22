1ac560c440e68c02dab78015038f037904e4d9bb
zhkl0228 <zhkl0228@qq.com>
2024-08-22 20:54:48 +0800

    Build wolfssl.

--- src/test/java/com/wolfssl/provider/jsse/ProviderTest.java
+++ src/test/java/com/wolfssl/provider/jsse/ProviderTest.java
@@ -1,11 +1,13 @@
-package com.wolfssl;
+package com.wolfssl.provider.jsse;
 
 import cn.hutool.core.net.DefaultTrustManager;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.wolfssl.provider.jsse.WolfSSLProvider;
+import com.wolfssl.WolfSSL;
+import com.wolfssl.WolfSSLContext;
+import com.wolfssl.WolfSSLException;
 import junit.framework.TestCase;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -14,9 +16,12 @@ import okhttp3.ResponseBody;
 import org.scijava.nativelib.NativeLoader;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.SecureRandom;
 import java.security.Security;
 
 public class ProviderTest extends TestCase {
@@ -41,7 +46,7 @@ public class ProviderTest extends TestCase {
         X509TrustManager trustManager = new DefaultTrustManager();
         context.init(null, new TrustManager[]{trustManager}, null);
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        builder.sslSocketFactory(context.getSocketFactory(), trustManager);
+        builder.sslSocketFactory(createSSLSocketFactory(), trustManager);
         OkHttpClient client = builder.build();
         Request request = new Request.Builder().url("https://tls.browserleaks.com/json").build();
         try (Response response = client.newCall(request).execute()) {
@@ -51,6 +56,17 @@ public class ProviderTest extends TestCase {
             JSONObject obj = JSON.parseObject(json, Feature.OrderedField);
             System.out.println(obj.toString(SerializerFeature.PrettyFormat));
         }
+    }
+
+    private SSLSocketFactory createSSLSocketFactory() throws WolfSSLException, KeyManagementException {
+        com.wolfssl.WolfSSLContext context = new WolfSSLContext(WolfSSL.TLSv1_3_Method());
+        context.setCipherList("TLS_AES_128_GCM_SHA256");
+//        context.set1SigAlgsList("RSA+SHA256:ECDSA:SHA256");
+        WolfSSLAuthStore authStore = new WolfSSLAuthStore(null, new TrustManager[]{new DefaultTrustManager()}, new SecureRandom(), WolfSSL.TLS_VERSION.TLSv1_3);
+        WolfSSLParameters parameters = new WolfSSLParameters();
+        parameters.setProtocols(new String[]{"TLSv1.3", "TLSv1.2"});
+        parameters.setCipherSuites(new String[]{"TLS_AES_128_GCM_SHA256"});
+        return new WolfSSLSocketFactory(context, authStore, parameters);
     }
 
 }
